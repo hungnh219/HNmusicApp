ce28a09b10712703365ba5e3a3241c2ff49117f4
Hoang Hung <hungnh219@gmail.com>
2024-06-14 23:07:52 +0700

    [bug] fix song bar bug

--- music_app/lib/components/songBar/song_bar.dart
+++ music_app/lib/components/songBar/song_bar.dart
@@ -20,58 +20,61 @@ class _SongBarState extends State<SongBar> {
       tag: 'songBar',
       child: Padding(
         padding: EdgeInsets.only(left: screenWidth * 0.02),
-          child: InkWell(
-            onTap: () {
-              print('123321');
-            },
-            child: Container(
-            // width: double.infinity,
-            height: 48,
-            width: screenWidth * 0.96,
-            decoration: BoxDecoration(
-              // color: Colors.yellow.withOpacity(0.8),
-              color: Color.fromRGBO(37, 37, 37, 0.9),
-              borderRadius: BorderRadius.circular(24),
-              border: Border.all(width: 1, color: Colors.white30)
-            ),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                IconButton(
-                  padding: EdgeInsets.zero,
-                  onPressed: () {},
-                  icon: Icon(
-                    color: Colors.white,
-                    Icons.play_circle,
-                    size: 32,  
-                  )
+          child: Material(
+            color: Colors.black.withOpacity(0),
+            child: InkWell(
+              onTap: () {
+                // print('123321');
+              },
+              child: Container(
+              // width: double.infinity,
+                height: 48,
+                width: screenWidth * 0.96,
+                decoration: BoxDecoration(
+                  // color: Colors.yellow.withOpacity(0.8),
+                  color: Color.fromRGBO(37, 37, 37, 0.9),
+                  borderRadius: BorderRadius.circular(24),
+                  border: Border.all(width: 1, color: Colors.white30)
                 ),
-                  
-                Expanded(
-                  flex: 4,
-                  child: Text('hungdsnfsdlfdsfsdfsfsfsfsfsdlkfjdsljfklsdjlfasdjl;fkdjkfjs')
-                ),
-                  
-                IconButton(
-                  padding: EdgeInsets.zero,
-                  onPressed: () {},
-                  icon: Icon(
-                    Icons.more_rounded,
-                    size: 24,  
-                  )
-                ),
-                  
-                IconButton(
-                  padding: EdgeInsets.zero,
-                  onPressed: () {},
-                  icon: Icon(
-                    Icons.heart_broken_outlined,
-                    size: 24,  
-                  )
-                ),
-              ],
-            ),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    IconButton(
+                      padding: EdgeInsets.zero,
+                      onPressed: () {},
+                      icon: Icon(
+                        color: Colors.white,
+                        Icons.play_circle,
+                        size: 32,  
+                      )
                     ),
+                      
+                    Expanded(
+                      flex: 4,
+                      child: Text('hungdsnfsdlfdsfsdfsfsfsfsfsdlkfjdsljfklsdjlfasdjl;fkdjkfjs')
+                    ),
+                      
+                    IconButton(
+                      padding: EdgeInsets.zero,
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.more_rounded,
+                        size: 24,  
+                      )
+                    ),
+                      
+                    IconButton(
+                      padding: EdgeInsets.zero,
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.heart_broken_outlined,
+                        size: 24,  
+                      )
+                    ),
+                  ],
+                ),
+              ),
+            ),
           ),
       ),
     );
